2ab85801b32372e6036af70061fca52cad6c74cd
sanjanabalakrishna <sanjanabalakrishna@gmail.com>
2019-10-08 21:49:09 -0400

    Update LenOfLongestSubstring.py

--- BasicPythonPractice/LenOfLongestSubstring.py
+++ BasicPythonPractice/LenOfLongestSubstring.py
@@ -1,12 +1,30 @@
 #Given a string, find the length of the longest substring without repeating characters
 class Solution:
-    def lenOfLongestSubstring(self,s):
-        str = ''
-        for i in range(0,len(s)-1):
-            if s[i] != s[i-1]:
-                str = str + s[i]
-            else:
-                break
-        return(len(str))
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l=[]
+        nl=[]
+        num=[]
+
+        if len(s)==0:
+            res = 0
+        for a in range(0,len(s)):
+            num.append(a)
+        
+        for j in num:
+            for i in range(j,len(s)):
+                if s[i] not in l:
+                    l.append(s[i])
+                else:
+                    #num.append(i)
+                    break
+            nl.append(len(''.join(l)))
+            l.clear()
+            res=max(nl)
+        return(res)
+        
     
-Solution().lenOfLongestSubstring('abrkaabcdefghijjxxx')
+Solution().lenOfLongestSubstring('abrkaabcdefghijjxxx') 
+
+#TestCase1: abrkaabcdefghijjxxx: 10
+#TestCase2: dvdf: 3
+#TestCase3: '': 0
